01ff30287bd9d074280d404305161e20d39c5794
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-24 20:39:27 +0530

    Add Editor and Form Component

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,16 @@ import Container from "./Container";
 import LogoutBtn from "./LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
-export { Header, Footer, Logo, Container, LogoutBtn, Button, Input };
+import RTE from "./RTE";
+import Select from "./Select";
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  RTE,
+  Select,
+};
