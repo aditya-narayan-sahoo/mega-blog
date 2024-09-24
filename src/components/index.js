a849b2a9494b04a494ecbaac47f7b09d561eed86
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-24 10:59:53 +0530

    Add Login and SignUp Components and AutheticationLayout

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ import Footer from "./Footer";
 import Logo from "./Logo";
 import Container from "./Container";
 import LogoutBtn from "./LogoutBtn";
-export { Header, Footer, Logo, Container, LogoutBtn };
+import Button from "./Button";
+import Input from "./Input";
+export { Header, Footer, Logo, Container, LogoutBtn, Button, Input };
